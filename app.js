22d9956e1e7224a445bd4292dc5df014cd6a060d
Кирилл Головин <ariunmlg@gmail.com>
2023-05-16 01:08:29 +0500

    14Пр
    Вернул к исходу

--- app.js
+++ app.js
@@ -4,12 +4,27 @@ const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const errorMiddleware = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./utils/logger');
-const router = require('./routes/router');
+
+const routerUsers = require('./routes/users');
+const routerCards = require('./routes/cards');
+const { NOT_FOUND_404 } = require('./utils/utils');
+const { login, createUsers } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { validateLogin, validateRegister } = require('./utils/validation');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(router);
+app.post('/signin', validateLogin, login);
+app.post('/signup', validateRegister, createUsers);
+
+app.use(auth);
+app.use('/users', routerUsers);
+app.use('/cards', routerCards);
+
+app.use((req, res) => {
+  res.status(NOT_FOUND_404).send({ message: 'Такой страницы не существует.' });
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
